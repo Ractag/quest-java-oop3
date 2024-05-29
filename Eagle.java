50cba69d7bf65abbab97720e1899b004e4d6d7ce
Victor G <victorgj95@outlook.com>
2024-05-29 09:28:53 +0200

    exercice done

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,60 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff() {
+        if (this.flying) {
+            System.out.println("Hawkeye lands on the ground.");
+        } else {
+            this.flying = true;
+            System.out.println("Hawkeye takes off in the sky.");
+        }
+    }
+
+    @Override
+    public int ascend(int altitude) {
+        while (this.altitude < 120) {
+            this.altitude += altitude;
+            if (this.altitude >= 120) {
+                System.out.println("Hawkeye flies upward, altitude: 120.");
+                break;
+            } else if (this.altitude <= 30) {
+                System.out.println("Hawkeye flies upward, altitude: 30.");
+            }
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void glide() {
+        System.out.println("Hawkeye glides.");
+    }
+
+    @Override
+    public int descend(int altitude) {
+        while (this.altitude > 0) {
+            this.altitude -= altitude;
+            if (this.altitude == 30) {
+                System.out.println("Hawkeye flies downward, altitude: 30.");
+                break;
+            } else if (this.altitude <= 9) {
+                System.out.println("Hawkeye flies downward, altitude: 9.");
+                break;
+            }
+
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude > 9) {
+            System.out.println("Hawkeye is too high to land yet.");
+        } else {
+            System.out.println("Hawkeye lands.");
+        }
+
+    }
 }
+
